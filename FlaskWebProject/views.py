137455c0dcf73e7da9f215e74c1acd189126eba3
NGUYỄN HỮU THẮNG <67738470+HuuThang-1402@users.noreply.github.com>
2023-03-16 14:59:57 +0700

    Update views.py
    update views

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -20,6 +20,8 @@ imageSourceUrl = 'https://'+ app.config['BLOB_ACCOUNT']  + '.blob.core.windows.n
 @login_required
 def home():
     user = User.query.filter_by(username=current_user.username).first_or_404()
+    if user:
+        app.logger.info('No issue.')
     posts = Post.query.all()
     return render_template(
         'index.html',
